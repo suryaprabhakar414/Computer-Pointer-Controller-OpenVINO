82d68c41b0a2550e121f9aac8de00993f935ce79
Surya Prabhakaran <suryaprabhakar41@gmail.com>
2020-06-30 13:15:28 +0530

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -4,6 +4,7 @@ import cv2
 from face_detection import Face_Detection
 from facial_landmarks_detection import Facial_Landmarks_Detection
 from head_pose_estimation import Head_Pose_Estimation
+from gaze_estimation import Gaze_Estimation
 
 
 Image = cv2.imread("/home/vegeta/Documents/image.png")
@@ -22,10 +23,16 @@ left_eye_image,right_eye_image,eye_coordinates = FLD.predict(image)
 
 HPE = Head_Pose_Estimation()
 HPE.load_model()
-print(HPE.get_input_shape())
-print(HPE.get_output_shape())
-result = HPE.predict(image)
-print(result)
+head_pose_angles = HPE.predict(image)
+
+#######################################################################################################################################################################
+
+
+GE = Gaze_Estimation()
+GE.load_model()
+mouse_coordinates,gaze_vector = GE.predict(left_eye_image,right_eye_image,head_pose_angles)
+
+########################################################################################################################################################################
 
 
 
