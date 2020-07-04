dd8ae661442bc7e65c1676de9fe4cf675de1fa0c
Surya Prabhakaran <suryaprabhakar41@gmail.com>
2020-07-04 12:37:14 +0530

    Update facial_landmarks_detection.py

--- src/facial_landmarks_detection.py
+++ src/facial_landmarks_detection.py
@@ -10,7 +10,7 @@ class Facial_Landmarks_Detection:
     '''
     Class for the Face Detection Model.
     '''
-    def __init__(self, device='CPU', extensions=None):
+    def __init__(self, device, extensions=None):
         '''
         TODO: Use this to set your instance variables.
         '''
@@ -20,20 +20,24 @@ class Facial_Landmarks_Detection:
         self.output_blob = None
         self.exec_network = None
         self.infer_request = None
+        self.device = device
+        self.extensions = extensions
 
-    def load_model(self):
+    def load_model(self,model_path):
         
-        self.network = IENetwork(model='/opt/intel/openvino_2020.2.120/deployment_tools/open_model_zoo/tools/downloader/intel/landmarks-regression-retail-0009/FP32/landmarks-regression-retail-0009.xml', weights='/opt/intel/openvino_2020.2.120/deployment_tools/open_model_zoo/tools/downloader/intel/landmarks-regression-retail-0009/FP32/landmarks-regression-retail-0009.bin')
+        model = model_path+"landmarks-regression-retail-0009.xml"
+        weights = model_path+"landmarks-regression-retail-0009.bin"
+        self.network = IENetwork(model=model,weights = weights)
         self.plugin = IECore()
-        self.exec_network = self.plugin.load_network(self.network,"CPU")
+        if self.extensions and "CPU" in self.device:
+            self.plugin.add_extension(self.extensions, self.device)
+        self.check_model()
+        self.exec_network = self.plugin.load_network(self.network,self.device)
         self.input_blob  = next(iter(self.network.inputs))
         self.output_blob = next(iter(self.network.outputs))
         
     def get_input_shape(self):
         return self.network.inputs[self.input_blob].shape
-
-    def get_output_shape(self):
-        return self.network.outputs[self.output_blob].shape
     
     def preprocess_input(self, image):
         input_shape = self.get_input_shape()
@@ -43,24 +47,29 @@ class Facial_Landmarks_Detection:
         return image
 
     def check_model(self):
-        raise NotImplementedError
-
-    
-    def inference(self, image):
-        #self.exec_network.start_async(request_id=0,inputs={self.input_blob:image.astype(np.float32)})
-        self.exec_network.infer(inputs={self.input_blob:image.astype(np.float32)})
-        return
-        
-    
-    def wait(self):
-        status = self.exec_network.requests[0].wait(-1)
-        return status
-    
-    def predict(self, image):
+        sl = self.plugin.query_network(network=self.network, device_name=self.device)
+        ul = [l for l in self.network.layers.keys() if l not in sl]
+        if len(ul) != 0:
+            print("Unsupported layers found: {}".format(ul))
+            print("Check whether extensions are available to add to IECore.")
+            exit(1)
+            
+    def predict(self, image, inference_type):
         image_FLD = self.preprocess_input(image)
-        self.inference(image_FLD)
-        result = self.exec_network.requests[0].outputs[self.output_blob][0]
+
+        if(inference_type=="async"):
+            self.exec_network.start_async(request_id=0,inputs={self.input_blob:image_FLD.astype(np.float32)})
+            status = self.exec_network.requests[0].wait(-1)
+            if status==0:
+                result = self.exec_network.requests[0].outputs[self.output_blob]
+            
+        else:
+            self.exec_network.infer(inputs={self.input_blob:image_FLD.astype(np.float32)})
+            result = self.exec_network.requests[0].outputs[self.output_blob]
+
+        result = np.reshape(result,(10,1))
         coordinates = self.preprocess_output(result,image,image.shape[0],image.shape[1])
+        
         left_eye_x_min = coordinates['left_eye'][0]-10
         left_eye_x_max = coordinates['left_eye'][0]+10
         left_eye_y_min = coordinates['left_eye'][1]-10
@@ -70,15 +79,11 @@ class Facial_Landmarks_Detection:
         right_eye_x_max = coordinates['right_eye'][0]+10
         right_eye_y_min = coordinates['right_eye'][1]-10
         right_eye_y_max = coordinates['right_eye'][1]+10
-
         
         left_eye_image = image[left_eye_x_min:left_eye_x_max, left_eye_y_min:left_eye_y_max]
         right_eye_image = image[right_eye_x_min:right_eye_x_max, right_eye_y_min:right_eye_y_max]
-        
-        eye_coordinates = {'left_eye':[left_eye_x_min,left_eye_y_min,left_eye_x_max,left_eye_y_max],
-        'right_eye':[right_eye_x_min,right_eye_y_min,right_eye_x_max,right_eye_y_max]}
 
-
+        eye_coordinates = coordinates
 
 
         return left_eye_image,right_eye_image,eye_coordinates
