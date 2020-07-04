97994d5664187492208a7af1c9bd2dadb1b03fdf
Surya Prabhakaran <suryaprabhakar41@gmail.com>
2020-07-04 09:56:11 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,6 +10,8 @@ from input_feeder import InputFeeder
 import argparse
 import math
 import time
+import logging
+
 
 def build_argparser():
 
@@ -22,7 +24,7 @@ def build_argparser():
     prob_desc = "Probability threshold for model to detect the face accurately from the video frame."
     d_desc = "Specify the target device to infer on: CPU, GPU, FPGA or MYRIAD is acceptable. Sample will look for a suitable plugin for device specified (CPU by default)"
     l_desc = "MKLDNN (CPU)-targeted custom layers. Absolute path to a shared library with the kernels impl."
-    
+    it_desc = "Inference type, sync for Synchronous or async for Asychronous"
     parser = argparse.ArgumentParser("Computer Pointer Controller")
 
     parser.add_argument("-i", required=True, type=str, help=i_desc)
@@ -34,43 +36,71 @@ def build_argparser():
     parser.add_argument("-prob", required=False, type=float,default=0.6,help=prob_desc)
     parser.add_argument("-d", type=str, default="CPU", help=d_desc)
     parser.add_argument("-l", required=False, type=str,default=None,help=l_desc)
+    parser.add_argument("-it",required = False,type = str, default = "sync",help = it_desc)
     args = parser.parse_args()
     return args
 
 def main():
+
+    try:
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s [%(levelname)s] %(message)s",
+            handlers=[
+                logging.FileHandler("Computer_Pointer_Controller.log"),
+                logging.StreamHandler()
+            ])
+    except:
+        print("File cannot be created")
+
+    
     args = build_argparser()
     video_path = args.i
     visualize = args.flags
     count=0
+    fd_inference_time = 0
+    fld_inference_time = 0
+    hp_inference_time = 0
+    ge_inference_time = 0
+    00
     MC = MouseController('medium','fast')
     
-    ##### LOADING MODELS #####
+    logging.info("############## Model Load Time #############")
+   
     start_time = time.time()
+    first_model_time = start_time
     FD = Face_Detection(device = args.d,threshold = args.prob, extensions=args.l)
     FD.load_model(model_path = args.f)
-
+    logging.info("Face Detection Model: {:.3f}ms".format(1000*(time.time()-first_model_time)))
+    
+    second_model_time = time.time()
     FLD = Facial_Landmarks_Detection(device = args.d, extensions=args.l)
     FLD.load_model(model_path = args.fl)
-    
+    logging.info("Facial Landmarks Detection Model: {:.3f}ms".format(1000*(time.time()-second_model_time)))
+
+    third_model_time = time.time()
     HPE = Head_Pose_Estimation(device = args.d, extensions=args.l)
     HPE.load_model(model_path = args.hp)
+    logging.info("Head Pose Estimation Model: {:.3f}ms".format(1000*(time.time()-third_model_time)))
 
+    fourth_model_time = time.time()
     GE = Gaze_Estimation(device = args.d, extensions=args.l)
     GE.load_model(model_path = args.g)
+    logging.info("Gaze Estimation Model: {:.3f}ms".format(1000*(time.time()-fourth_model_time)))
+    logging.info("############## End ######################### ")
 
-    total_load_time = (time.time()-start_time)*1000
-
-    print("MODEL LOADED SUCCESSFULLY")
+    Total_Model_Load_Time = 1000*(time.time()-start_time)
 
     ##### LOADING VIDEO FILE #####
+
     if(video_path=="cam"):
         IF = InputFeeder("cam")
     else:
         IF = InputFeeder("video",video_path)   
     IF.load_data()
-    print("VIDEO LOADED SUCCESSFULLY")
 
     ##### MODEL INFERENCE #####
+
     start_inf_time = time.time()
     for flag,frame in IF.next_batch():
         
@@ -79,15 +109,26 @@ def main():
         
         if(count%5==0):
             cv2.imshow('frame',cv2.resize(frame,(500,500)))
-    
+        
         key=cv2.waitKey(60)
         count = count+1
-        face,face_coordinates = FD.predict(frame)
-        left_eye_image,right_eye_image,eye_coordinates = FLD.predict(face)
-        head_pose_angles = HPE.predict(face)
-        mouse_coordinates,gaze_vector = GE.predict(left_eye_image,right_eye_image,head_pose_angles)
-        
-        
+
+        start_time_1 = time.time()
+        face,face_coordinates = FD.predict(frame, args.it)
+        fd_inference_time += (time.time()-start_time_1)
+
+        start_time_2 = time.time()
+        left_eye_image,right_eye_image,eye_coordinates = FLD.predict(face,args.it)
+        fld_inference_time += (time.time()-start_time_2)
+
+        start_time_3 = time.time()
+        head_pose_angles = HPE.predict(face,args.it)
+        hp_inference_time += (time.time()-start_time_3)
+
+        start_time_4 = time.time()
+        mouse_coordinates,gaze_vector = GE.predict(left_eye_image,right_eye_image,head_pose_angles,args.it)
+        ge_inference_time += (time.time()-start_time_4)
+
         if(len(visualize)!=0):
             frame_visualize = frame.copy()
             
@@ -118,49 +159,32 @@ def main():
                              (int(eye_coordinates['left_eye'][0] + arrow_X), int(eye_coordinates['left_eye'][1] + arrow_Y)),(255,0,0),2)
                 cv2.arrowedLine(frame_visualize,(eye_coordinates['right_eye'][0],eye_coordinates['right_eye'][1]),
                              (int(eye_coordinates['right_eye'][0] + arrow_X), int(eye_coordinates['right_eye'][1] + arrow_Y)),(255,0,0),2)
-                
-            
+                  
             cv2.imshow('Visualization',cv2.resize(frame_visualize,(500,500)))
-
+        
         if(count%5==0):
             MC.move(mouse_coordinates[0],mouse_coordinates[1])
         
         if key==27:
             break
-    print(count)
-    inference_time = (time.time()-start_inf_time) 
-    fps = round(count/inference_time,1)
-    print(total_load_time, inference_time, fps)
+
+    Total_Inference_Time = time.time()-start_inf_time
+    if(count>0):
+        logging.info("############## Models Inference time #######") 
+        logging.info("Face Detection:{:.3f}ms".format(1000*fd_inference_time/count))
+        logging.info("Facial Landmarks Detection:{:.3f}ms".format(1000*fld_inference_time/count))
+        logging.info("Headpose Estimation:{:.3f}ms".format(1000*hp_inference_time/count))
+        logging.info("Gaze Estimation:{:.3f}ms".format(1000*ge_inference_time/count))
+        logging.info("############## End #########################") 
+    
+    logging.info("############## Summarized Results ##########")
+    logging.info("Total Model Load_ ime: {:.3f}ms".format(Total_Model_Load_Time))
+    logging.info("Total Inference Time: {:.3f}ms".format(Total_Inference_Time))
+    logging.info("FPS:{}".format(int(count/Total_Inference_Time)))
+    logging.info("############ End ###########################") 
+    cv2.destroyAllWindows()
+    IF.close()  
+    
 
 if __name__ == '__main__':
-    
     main()
-
-
-
-
-    
-
-
-
-
-
-
-
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
