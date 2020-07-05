b1ea7c6becd4bed694faa9b27fbce7faba1f4a5a
Surya Prabhakaran <suryaprabhakar41@gmail.com>
2020-07-05 13:40:16 +0530

    Update input_feeder.py

--- src/input_feeder.py
+++ src/input_feeder.py
@@ -36,9 +36,9 @@ class InputFeeder:
         '''
         while True:
             for _ in range(10):
-                _, frame=self.cap.read()
-            yield frame
-
+                flag, frame=self.cap.read()
+            yield flag, frame
+    
 
     def close(self):
         '''
